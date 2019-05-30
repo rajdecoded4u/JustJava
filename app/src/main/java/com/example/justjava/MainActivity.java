70e4ccef4ad96fec7d08e90cfad74e6bacd05bd9
rajdecoded4u <rajchanchal2102@gmail.com>
2019-05-30 11:29:49 +0100

    Added :- Mail Support

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.justjava;
 
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,6 +18,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
     int quantity =0;
+    String summary;
+    String custName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,26 +35,58 @@ public class MainActivity extends AppCompatActivity {
         String name = text.getText().toString();
         CheckBox wippedCreamCheckBox = (CheckBox) findViewById(R.id.check_box);
         Boolean hasWhippedCream = wippedCreamCheckBox.isChecked();
+
         CheckBox ChocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_box);
         Boolean hasChocolate = ChocolateCheckBox.isChecked();
 
-        int price = calculatePrice();
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
         String priceMessage = onOrderSummary(name,price,hasWhippedCream,hasChocolate);
+        summary=priceMessage;
+        custName=name;
+
+
         displayMessage(priceMessage);
+    }
+    public void mailTo(View view) {
+        EditText text = (EditText)findViewById(R.id.user_address);
+        String address = text.getText().toString();
+        summary +="\n Deliver To :-"+address;
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Placing Order for "+custName);
+        intent.putExtra(Intent.EXTRA_TEXT, summary);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+
     }
 
     public void increment(View view){
+        if(quantity<99){
         quantity = quantity+1;
-        display(quantity);
+        display(quantity);}
+        else
+            return;
 
     }
 
     public void decrement(View view){
-        quantity = quantity-1;
-        display(quantity);
+        if(quantity>1) {
+            quantity = quantity - 1;
+            display(quantity);
+        }
+        else
+            return;
     }
-    private int  calculatePrice(){
-        return 5*quantity;
+    private int  calculatePrice(boolean cream, boolean chocolate){
+        int rate=5;
+        if(cream==true)
+            rate+=1;
+        if (chocolate==true)
+            rate+=2;
+        return (quantity*rate);
+
     }
 
     private String onOrderSummary(String name,int price, Boolean hasWhippedCream, Boolean hasChocolate){
